05bc92595f2b7e63bd12fdbfbda2e5a45f9d2047
Hauke D <haukex@zero-g.net>
2024-07-07 10:40:22 +0000

    Added some match scoring

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   const result_rows = document.getElementById('result_rows') as HTMLElement
   const result_count = document.getElementById('result_count') as HTMLElement
   const load_fail = document.getElementById('dict-load-fail') as HTMLElement
-  const no_results = result_rows.children[0] as HTMLElement  // should be a tr
+  const no_results = (result_rows.children[0] as HTMLElement).cloneNode(true) as HTMLElement  // should be a tr
 
   const dictResp = await loadDict()
   if ( !dictResp || !dictResp.ok ) {
@@ -45,29 +45,34 @@ window.addEventListener('DOMContentLoaded', async () => {
     throw new Error('Could not load dictionary.')
   }
 
-  // these two replaces fix some oversights that I guess happened on conversion from CP1252 to UTF-8 (?)
-  const dictLines = (await dictResp.text()).replaceAll('\u0092','\u2019').replaceAll('\u0096','\u2013')
+  const dictLines = (await dictResp.text())
+    // these two replaces fix some oversights that I guess happened on conversion from CP1252 to UTF-8 (?)
+    .replaceAll(String.fromCodePoint(0x92),'\u2019').replaceAll(String.fromCodePoint(0x96),'\u2013')
     .split(/\r?\n|\r(?!\n)/g).map((line) => line.trim()).filter((line) => line.length && !line.startsWith('#'))
 
   const do_search = () => {
-    const whatRe = new RegExp(escapeStringRegexp(search_term.value.trim()), 'ig')
-    const matches = search_term.value.trim().length ? dictLines.filter((line) => line.match(whatRe)) : []
-    //TODO: sort matches; prioritize matches at beginning of word
-    result_rows.replaceChildren()
-    let displayCount = 0
-    matches.slice(0,MAX_RESULTS).forEach((match) => {
-      displayCount++
-      const trans = match.split(/::/)
+    const whatPat = escapeStringRegexp(search_term.value.trim())
+    const scoreRes :RegExp[] = [ `(?:^|::\\s*)${whatPat}`, `(?:^|::\\s*|\\|\\s*)${whatPat}`, `\\b${whatPat}` ]
+      .flatMap((re)=>[re,re+'\\b']).flatMap((re)=>[new RegExp(re), new RegExp(re, 'i')])
+    const whatRe = new RegExp(whatPat, 'ig')
+    const scoredMatches :[string,number][] = (
+      search_term.value.trim().length ? dictLines.filter((line) => line.match(whatRe)) : [] )
+      .map((matchedLine) => [matchedLine, scoreRes.map((re):number=>matchedLine.match(re)?1:0).reduce((a,b)=>a+b,0) ])
+    scoredMatches.sort((a,b) => b[1]-a[1])  // should be stable in modern JS
+    const newChildren :HTMLElement[] = []
+    scoredMatches.slice(0, MAX_RESULTS).forEach((scoredMatch) => {
+      const trans = scoredMatch[0].split(/::/)
       if (trans.length!=2)
-        throw new Error(`unexpected database format on line "${match}"`)
+        throw new Error(`unexpected database format on line "${scoredMatch[0]}"`)
       const des = (trans[0] as string).split(/\|/)
       const ens = (trans[1] as string).split(/\|/)
       if (des.length!=ens.length)
-        throw new Error(`unexpected database format on line "${match}"`)
+        throw new Error(`unexpected database format on line "${scoredMatch[0]}"`)
       const tr = document.createElement('tr')
       const td0 = document.createElement('td')
       const td1 = document.createElement('td')
       des.map((de, i) => {
+        //TODO Later: if a line wraps, the following pairs will no longer be aligned
         const en = ens[i] as string
         const div0 = document.createElement('div')
         if (i) div0.classList.add('sub-result')
@@ -82,16 +87,17 @@ window.addEventListener('DOMContentLoaded', async () => {
       })
       tr.appendChild(td0)
       tr.appendChild(td1)
-      result_rows.appendChild(tr)
+      newChildren.push(tr)
     })
-    if (!matches.length) {
+    if (!scoredMatches.length) {
       result_count.innerText = `No matches found (dictionary holds ${dictLines.length} entries).`
-      result_rows.appendChild(no_results)
+      newChildren.push(no_results.cloneNode(true) as HTMLElement)
     }
-    else if (displayCount!=matches.length)
-      result_count.innerText = `Found ${matches.length} matches, showing the first ${displayCount}.`
+    else if (newChildren.length!=scoredMatches.length)
+      result_count.innerText = `Found ${scoredMatches.length} matches, showing the first ${newChildren.length}.`
     else
-      result_count.innerText = `Showing all ${matches.length} matches.`
+      result_count.innerText = `Showing all ${scoredMatches.length} matches.`
+    result_rows.replaceChildren(...newChildren)
   }
 
   search_term.addEventListener('change', do_search)
